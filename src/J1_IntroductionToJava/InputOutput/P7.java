bff6eb0155b3d018b5e93782db5a8ad3a2b5d53f
RiyaGahtori <riyagahtori21@gmail.com>
2022-05-12 16:23:59 +0530

    Commit

--- src/J1_IntroductionToJava/InputOutput/P7.java
+++ src/J1_IntroductionToJava/InputOutput/P7.java
@@ -1,7 +1,23 @@
 //7.	Write a program to enter length in centimeter and convert it into meter and kilometer.
- /**/
+ /*
+ 1m = 100cm
+ 1km = 10000cm
+ */
 
 package J1_IntroductionToJava.InputOutput;
+import java.util.Scanner;
 
 public class P7 {
+    public static void main (String[] args){
+        float l, meter, kilometer;
+        Scanner length = new Scanner(System.in);
+        System.out.print("Enter the length in centimeter : ");
+        l = length.nextFloat();
+        meter = (l/100);
+        kilometer = (l/100000);
+        System.out.println("___________________________________________");
+        System.out.println("Length in Meter      = " + meter + " meter");
+        System.out.println("Length in Kilometer  = " + kilometer + " kilometer");
+        System.out.println("___________________________________________");
+    }
 }
