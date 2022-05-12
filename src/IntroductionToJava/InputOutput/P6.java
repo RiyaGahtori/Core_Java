793d65d26c862800dc459d862f561f326e5a52e0
RiyaGahtori <riyagahtori21@gmail.com>
2022-05-12 15:45:55 +0530

    Commit

--- src/IntroductionToJava/InputOutput/P6.java
+++
@@ -1,8 +0,0 @@
-/*6.	Write a program to enter radius of a circle and find its diameter, circumference and area.
-
-
- */
-package IntroductionToJava.InputOutput;
-
-public class P6 {
-}
---
+++ src/J1_IntroductionToJava/InputOutput/P6.java
@@ -0,0 +1,28 @@
+//6.	Write a program to enter radius of a circle and find its diameter, circumference and area.
+ /*
+Formula:                                   where:  r = radius of the circle
+D = 2 * r                                          D = diameter of the circle
+C = 2 * PI * r                                     C = circumference of the circle
+A = PI * r^2                                       A = area of the circle
+--> PI = 3.14
+*/
+package J1_IntroductionToJava.InputOutput;
+import java.util.Scanner;
+
+public class P6 {
+    public static void main(String[] args){
+        float r,diameter,circumference,area;
+        float pi= 3.14f ;
+        Scanner radius = new Scanner(System.in);
+        System.out.print("Enter the radius of circle : ");
+        r = radius.nextFloat();
+        System.out.println("________________________________________");
+        diameter=2*(r);
+        circumference = 2*pi*r;
+        area = pi*(r*r);
+        System.out.println("Diameter of circle is : " +diameter + " units");
+        System.out.println("Circumference of circle is : " +circumference + " units");
+        System.out.println("Area of circle is : " + area + " sq. units");
+        System.out.println("________________________________________");
+    }
+}
