67f87ddaee6bec4daa65bff69c51e68678f2562f
RiyaGahtori <riyagahtori21@gmail.com>
2022-05-12 16:59:50 +0530

    Commit

---
+++ src/J1_IntroductionToJava/InputOutput/P9.java
@@ -0,0 +1,21 @@
+//9.	Write a program to enter temperature in Fahrenheit and convert to Celsius
+/*
+Formula :
+T(oC)   =  ((T(oF)  -  32  ) ×  5)/9
+ */
+
+package J1_IntroductionToJava.InputOutput;
+import java.util.Scanner;
+
+public class P9 {
+    public static void main(String[] args){
+        float F, C;
+        Scanner Temperature = new Scanner(System.in);
+        System.out.print("Enter the temperature in Fahrenheit : ");
+        F = Temperature.nextFloat();
+        System.out.println("___________________________________________");
+        C  = ((F-32)*5)/9;
+        System.out.println("Temperature in Celsius is : " + C + " oC");
+        System.out.println("___________________________________________");
+    }
+}
