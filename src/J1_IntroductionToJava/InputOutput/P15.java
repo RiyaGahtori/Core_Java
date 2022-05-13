6ba01e70d97f38b601aa88cb2d225e527584e3ba
RiyaGahtori <riyagahtori21@gmail.com>
2022-05-13 14:21:13 +0530

    Commit

---
+++ src/J1_IntroductionToJava/InputOutput/P15.java
@@ -0,0 +1,23 @@
+//15.	Write a program to calculate area of an equilateral triangle.
+/*
+A triangle is having all angles of 60 degrees and congruent sides is called Equilateral Triangle.
+Area of Equilateral Triangle = (1.73 * a* a)/4
+Where: a = side
+ */
+
+package J1_IntroductionToJava.InputOutput;
+import java.util.Scanner;
+
+public class P15 {
+    public static void main(String[] args){
+        float a;
+        double area;
+        Scanner side = new Scanner(System.in);
+        System.out.print("Enter side of Triangle : ");
+        a = side.nextFloat();
+        System.out.println("_________________________________");
+        area = (1.73*a*a)/4;
+        System.out.println("Area of Equilateral Triangle is : "+area);
+        System.out.println("_________________________________");
+    }
+}
