e57b1ca273502a82ebc5312dc7bb875f59523325
RiyaGahtori <riyagahtori21@gmail.com>
2022-05-13 06:45:41 +0530

    Commit

---
+++ src/J1_IntroductionToJava/InputOutput/P11.java
@@ -0,0 +1,23 @@
+//11.	Write a program to find power of any number x ^ y.
+/*
+Formula:
+result = base value ^ exponent value
+ */
+
+package J1_IntroductionToJava.InputOutput;
+import java.util.Scanner;
+
+public class P11 {
+    public static void main(String[] args){
+        int r, b, y;
+        Scanner by = new Scanner(System.in);
+        System.out.print("Enter the Base value : ");
+        b = by.nextInt();
+        System.out.print("Enter the Exponent value : ");
+        y = by.nextInt();
+        System.out.println("__________________________________");
+        r = (int)Math.pow(b,y);
+        System.out.println("The power of number "+ b+ "^" +y +" is : " + r);
+        System.out.println("__________________________________");
+    }
+}
