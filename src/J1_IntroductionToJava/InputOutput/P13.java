6075f278dc464655817d341373924742bf8b8f4b
RiyaGahtori <riyagahtori21@gmail.com>
2022-05-13 07:19:52 +0530

    Commit

---
+++ src/J1_IntroductionToJava/InputOutput/P13.java
@@ -0,0 +1,23 @@
+//13.	Write a program to enter two angles of a triangle and find the third angle.
+/*
+--> a° + b° + c° = 180°
+Formula:
+c° = 180° - (a° + b°)
+ */
+
+package J1_IntroductionToJava.InputOutput;
+import java.util.Scanner;
+
+public class P13 {
+    public static void main(String[] args){
+        int a, b, c;             //a = 1st angle, b = 2nd angle, c = 3rd angle
+        Scanner ab = new Scanner(System.in);
+        System.out.print("Enter First angle of triangle abc : ");
+        a = ab.nextInt();
+        System.out.print("Enter Second angle of triangle abc : ");
+        b = ab.nextInt();
+        System.out.println("______________________________________");
+        c = 180 - (a+b);
+        System.out.println("Third angle of the triangle : " +c);
+    }
+}
