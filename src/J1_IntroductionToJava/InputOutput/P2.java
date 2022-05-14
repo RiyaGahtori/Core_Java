262374f0b3df53b9a2f1bc55a867e908b7b08b45
RiyaGahtori <riyagahtori21@gmail.com>
2022-05-14 19:49:57 +0530

    Commit

--- src/J1_IntroductionToJava/InputOutput/P2.java
+++ src/J1_IntroductionToJava/InputOutput/P2.java
@@ -8,14 +8,14 @@ public class P2 {
         int num1 = 18, num2 = 3, addition;
         addition = num1 + num2;
         System.out.println("The sum of numbers is : " + addition);
-        System.out.println("-------------------------------------");
+        System.out.println("---------------------------------------");
 
         //By Using User-defined Method
         int x, y, sum;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the first number: ");
+        System.out.print("Enter the first number: ");
         x = sc.nextInt();
-        System.out.println("Enter the second number: ");
+        System.out.print("Enter the second number: ");
         y = sc.nextInt();
         sum = sum(x, y);
         System.out.println("The sum of two numbers x and y is: " + sum);
