595fa21a12f7fd23b8c12eed100eb4b02fa3d121
RiyaGahtori <riyagahtori21@gmail.com>
2022-05-16 09:52:12 +0530

    Commit

---
+++ src/J1_IntroductionToJava/CJ1_BasicsofJava/B1_JdkJreJvm.java
@@ -0,0 +1,28 @@
+/*
+1. Java Development Kit (JDK)
+JDK is a software development environment used for developing Java applications and applets. It physically exists.
+It contains -
+-- development tools(to provide an environment to develop your java programs).
+-- JRE(to execute your java program)
+
+2. Java Runtime Environment (JRE)
+JRE is an installation package that provides an environment to only run(not develop) the java program(or application)onto your machine. It physically exists.
+JRE is only used by those who only want to run Java programs that are end-users of your system.
+It contains -
+-- set of libraries
+-- other files that JVM uses at runtime.
+
+3. Java Virtual Machine (JVM)
+JVM is a very important part of both JDK and JRE because it is contained or inbuilt in both. It is called a virtual machine because it doesn't physically exist.
+Whatever Java program you run using JRE or JDK goes into JVM and JVM is responsible for executing the java program line by line, hence it is also known as an interpreter.
+The JVM performs the following main tasks:
+-Loads code
+-Verifies code
+-Executes code
+-Provides runtime environment
+ */
+
+package J1_IntroductionToJava.CJ1_BasicsofJava;
+
+public class B1_JdkJreJvm {
+}
