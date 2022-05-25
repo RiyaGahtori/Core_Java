9522b2151d73bff7f5b4e0bb75c272b154d6a9ac
RiyaGahtori <riyagahtori21@gmail.com>
2022-05-25 13:23:10 +0530

    Commit

---
+++ src/J1_IntroductionToJava/CJ3_DataTypes/P7.java
@@ -0,0 +1,13 @@
+//7. Write a program to add an integer variable having value 5 and a double variable having value 6.2.
+
+package J1_IntroductionToJava.CJ3_DataTypes;
+
+public class P7
+{
+    public static void main(String[] args)
+    {
+        int a = 5;
+        double b = 6.2;
+        System.out.println(a+b);
+    }
+}
