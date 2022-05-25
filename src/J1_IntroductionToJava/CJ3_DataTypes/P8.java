fdc8c58de341b0db87ac5a2c2a8423bd25408d7b
RiyaGahtori <riyagahtori21@gmail.com>
2022-05-25 13:26:34 +0530

    Commit

---
+++ src/J1_IntroductionToJava/CJ3_DataTypes/P8.java
@@ -0,0 +1,22 @@
+//8. Write a program to find the square of the number 3.9.
+
+package J1_IntroductionToJava.CJ3_DataTypes;
+
+public class P8
+{
+    public static void main(String[] args)
+    {
+        float a, b, power;
+        a = (float) 3.9;
+        b = 2;
+        power = (float)Math.pow(a,b);
+
+        System.out.println(power);//using Math.pow
+        System.out.println(" ");
+
+        System.out.println("_____________________");
+
+        System.out.println(" ");
+        System.out.println(3.9*3.9);
+    }
+}
