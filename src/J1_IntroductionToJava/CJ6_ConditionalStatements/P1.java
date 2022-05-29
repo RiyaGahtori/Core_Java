f5fa3eedff6dedcbfb695180ed19e812cf254ba8
RiyaGahtori <riyagahtori21@gmail.com>
2022-05-29 21:43:49 +0530

    Commit

---
+++ src/J1_IntroductionToJava/CJ6_ConditionalStatements/P1.java
@@ -0,0 +1,29 @@
+/*
+1. Write a program to accept two integers and check whether they are equal or not.
+Test Data : 21 21
+Expected Output : Number1 and Number2 are equal
+ */
+
+package J1_IntroductionToJava.CJ6_ConditionalStatements;
+import java.util.Scanner;
+
+public class P1 {
+    public static void main(String[] args)
+    {
+        Scanner num12 = new Scanner(System.in);
+        System.out.print("Enter The First number : ");
+        int Number1 = num12.nextInt();
+        System.out.print("Enter The Second number : ");
+        int Number2 = num12.nextInt();
+
+        if(Number1 == Number2)
+        {
+            System.out.println("Number1 and Number2 are equal ");
+        }
+        else
+        {
+            System.out.println("Number1 and Number2 are not equal");
+        }
+
+    }
+}
