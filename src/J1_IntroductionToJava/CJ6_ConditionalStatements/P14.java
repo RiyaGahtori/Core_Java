3ee39715a2a263c4ddcf9909d99353421f6cb543
RiyaGahtori <riyagahtori21@gmail.com>
2022-05-30 15:39:24 +0530

    Commit

---
+++ src/J1_IntroductionToJava/CJ6_ConditionalStatements/P14.java
@@ -0,0 +1,31 @@
+/*
+14. Write a program to check whether a triangle is Equilateral, Isosceles or Scalene.
+Test Data : 50 50 60
+Expected Output : This is an isosceles triangle.
+ */
+
+package J1_IntroductionToJava.CJ6_ConditionalStatements;
+import java.util.Scanner;
+
+public class P14 {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter sides of Triangle : ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        // Check for equilateral triangle
+        if (a == b && b == c )
+            System.out.println("This is an Equilateral Triangle");
+
+            // Check for isosceles triangle
+        else if (a == b || b == c || c == a )
+            System.out.println("This is an Isosceles Triangle");
+
+            // Otherwise scalene triangle
+        else
+            System.out.println("This is an Scalene Triangle");
+
+    }
+}
