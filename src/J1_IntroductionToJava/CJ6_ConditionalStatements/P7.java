8834b4a169cd6dc495c9afe337924471903d72fa
RiyaGahtori <riyagahtori21@gmail.com>
2022-05-30 07:31:15 +0530

    Commit

---
+++ src/J1_IntroductionToJava/CJ6_ConditionalStatements/P7.java
@@ -0,0 +1,35 @@
+/*
+7. Write a program to accept the height of a person in centimeter and categorize the person according to their height.
+Test Data : 135
+Expected Output :
+The person is Dwarf.
+ */
+
+package J1_IntroductionToJava.CJ6_ConditionalStatements;
+import java.util.Scanner;
+
+public class P7
+{
+    public static void main(String[] args)
+    {
+        Scanner sc =  new Scanner(System.in);
+        System.out.print("Enter You height in centimeter : " );
+        int height = sc.nextInt();
+
+        if(height >= 175)
+        {
+            System.out.println("You are beautiful and tall");
+        }
+        else if(height > 155)
+        {
+            System.out.println("Your height is average");
+        }
+        else
+        {
+            System.out.println("You are cute and short");
+        }
+        System.out.println(" ");
+        System.out.println(" Thank You! ");
+        System.out.println("Have a glorious day and a blessed week!");
+    }
+}
