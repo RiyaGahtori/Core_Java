0351992db64934fa67056cb4b43860caa22db5c6
RiyaGahtori <riyagahtori21@gmail.com>
2022-06-03 15:36:16 +0530

    Commit

---
+++ src/J1_IntroductionToJava/CJ7_LoopsInJava/P1.java
@@ -0,0 +1,9 @@
+// 1.	Write a program to print all natural numbers from 1 to n.
+
+package J1_IntroductionToJava.CJ7_LoopsInJava;
+
+public class P1 {
+    public static void main(String[] args){
+
+    }
+}
