b188895f2ec9a9bbc3ae38f63d3a2dd0d933ea23
RiyaGahtori <riyagahtori21@gmail.com>
2022-06-03 09:58:31 +0530

    Commit

---
+++ src/J1_IntroductionToJava/CJ5_TokenInJava/T3_Identifiers.java
@@ -0,0 +1,22 @@
+package J1_IntroductionToJava.CJ5_TokenInJava;
+
+public class T3_Identifiers {
+        public static void main(String[] args) {
+            int var1 = 99;
+            double var2 = 2.0;
+            System.out.println("Hello World!");
+        }
+    }
+
+/*
+=> Identifiers in the above sample code :
+T3_Identifiers (Class name)
+main (Method name)
+String (Predefined Class name)
+args (String variable name)
+var1 (integer variable name)
+var2 (double variable name)
+System(Predefined Class name)
+out(Variable name)
+println (Method name)
+*/
