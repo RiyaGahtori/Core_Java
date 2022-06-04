20e4935014840923761c6631e8cab60ff8c95e23
RiyaGahtori <riyagahtori21@gmail.com>
2022-06-04 06:39:36 +0530

    Commit

---
+++ src/J1_IntroductionToJava/CJ7_LoopsInJava/P4.java
@@ -0,0 +1,18 @@
+// 4.	Write a program to print all even numbers between 1 and 100.
+
+package J1_IntroductionToJava.CJ7_LoopsInJava;
+
+public class P4
+{
+    public static void main(String[] args)
+    {
+        System.out.println("List of even numbers from 1 to 100 : ");
+        for(int i=1; i<=100; i++)
+        {
+            if(i%2==0)
+            {
+                System.out.print(i + ("  "));
+            }
+        }
+    }
+}
