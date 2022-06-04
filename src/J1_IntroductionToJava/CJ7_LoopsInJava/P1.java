d84d9b040f7225e1e0ec047f429e131c495a52b3
RiyaGahtori <riyagahtori21@gmail.com>
2022-06-04 06:38:56 +0530

    Commit

--- src/J1_IntroductionToJava/CJ7_LoopsInJava/P1.java
+++ src/J1_IntroductionToJava/CJ7_LoopsInJava/P1.java
@@ -1,9 +1,18 @@
-// 1.	Write a program to print all natural numbers from 1 to n.
+// 1.	Write a program to print all natural numbers (from 1 to n).
 
 package J1_IntroductionToJava.CJ7_LoopsInJava;
+import java.util.Scanner;
 
 public class P1 {
     public static void main(String[] args){
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n : ");
+        n = sc.nextInt();
 
+        for (int i=1 ; i<=n; i++)
+        {
+            System.out.println(i);
+        }
     }
 }
