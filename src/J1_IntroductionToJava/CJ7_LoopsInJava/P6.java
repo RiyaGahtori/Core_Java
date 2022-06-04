afb4ceda17697fc0130a873ecdb539122cd2b833
RiyaGahtori <riyagahtori21@gmail.com>
2022-06-04 14:24:56 +0530

    Commit

---
+++ src/J1_IntroductionToJava/CJ7_LoopsInJava/P6.java
@@ -0,0 +1,20 @@
+// 6.	Write a program to find sum of all natural numbers between 1 to n.
+
+package J1_IntroductionToJava.CJ7_LoopsInJava;
+import java.util.Scanner;
+
+public class P6 {
+    public static void main(String[] args)
+    {
+        int n, i, sum = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n : ");
+        n = sc.nextInt();
+
+        for(i= 1 ; i<=n;i++)
+        {
+            sum = sum + i;
+        }
+        System.out.println("\n The Sum of Natural Numbers from 1 to "+ n + " = " + sum);
+    }
+}
