0af27b7dd82fbe4efdefc250879b6912c1ccb674
RiyaGahtori <riyagahtori21@gmail.com>
2022-06-04 06:39:25 +0530

    Commit

---
+++ src/J1_IntroductionToJava/CJ7_LoopsInJava/P3.java
@@ -0,0 +1,13 @@
+// 3.	Write a program to print all alphabets from a to z.
+
+package J1_IntroductionToJava.CJ7_LoopsInJava;
+
+public class P3
+{
+    public static void main(String[] args)
+    {
+        char c;
+        for(c = 'a'; c <= 'z'; ++c)
+            System.out.print(c + "  ");
+    }
+}
