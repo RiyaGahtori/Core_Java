a42353bd991f33c9e16096e3246f4243ef868e7e
RiyaGahtori <riyagahtori21@gmail.com>
2022-06-04 15:02:05 +0530

    Commit

---
+++ src/J1_IntroductionToJava/CJ7_LoopsInJava/P8.java
@@ -0,0 +1,24 @@
+// 8.	Write a program to find sum of all odd numbers between 1 to n.
+
+package J1_IntroductionToJava.CJ7_LoopsInJava;
+import java.util.Scanner;
+
+public class P8
+{
+    public static void main(String[] args)
+    {
+        int n, i,sum=0 ;
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Enter the value of n : ");
+        n = sc.nextInt();
+
+        for (i=0;i<=n;i++)
+        {
+            if(i%2 != 0)
+            {
+                sum += i;
+            }
+        }
+        System.out.println("Sum of all odd numbers from 1 to "+n +" is : "+sum);
+    }
+}
