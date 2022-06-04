bd878515715c883f343d3677b1cc4b6eab1aa660
RiyaGahtori <riyagahtori21@gmail.com>
2022-06-04 14:35:26 +0530

    Commit

---
+++ src/J1_IntroductionToJava/CJ7_LoopsInJava/P7.java
@@ -0,0 +1,24 @@
+//7.	Write a program to find sum of all even numbers between 1 to n.
+
+package J1_IntroductionToJava.CJ7_LoopsInJava;
+import java.util.Scanner;
+
+public class P7
+{
+    public static void main(String[] args)
+    {
+        int n, i,sum=0 ;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n : ");
+        n = sc.nextInt();
+
+        for (i=0;i<=n;i++)
+        {
+            if(i%2==0)
+            {
+                sum += i;
+            }
+        }
+        System.out.println("Sum of all even numbers from 1 to "+n +" is : "+sum);
+    }
+}
