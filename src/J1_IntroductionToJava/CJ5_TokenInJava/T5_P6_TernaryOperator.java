f6cd30623b2eeff6500cd99b116822cb275f41ec
RiyaGahtori <riyagahtori21@gmail.com>
2022-06-06 10:55:46 +0530

    Commit

---
+++ src/J1_IntroductionToJava/CJ5_TokenInJava/T5_P6_TernaryOperator.java
@@ -0,0 +1,10 @@
+package J1_IntroductionToJava.CJ5_TokenInJava;
+
+public class T5_P6_TernaryOperator {
+    public static void main(String[] args)
+    {
+        int a = 10, b = 20, Max;
+        Max = (a>b) ? a : b;
+        System.out.println(Max);
+    }
+}
