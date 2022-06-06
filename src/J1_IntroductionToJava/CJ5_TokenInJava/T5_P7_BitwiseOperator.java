d54f070ad732cea8515de17eac47a26dcab4c708
RiyaGahtori <riyagahtori21@gmail.com>
2022-06-06 14:51:16 +0530

    Commit

---
+++ src/J1_IntroductionToJava/CJ5_TokenInJava/T5_P7_BitwiseOperator.java
@@ -0,0 +1,13 @@
+package J1_IntroductionToJava.CJ5_TokenInJava;
+
+public class T5_P7_BitwiseOperator {
+    public static void main(String[] args)
+    {
+        int a=5,b=7;
+
+        System.out.println("AND : "+(a&b));
+        System.out.println("OR : "+(a|b));
+        System.out.println("XOR : "+(a^b));
+        System.out.println("Complement : "+(~a));
+    }
+}
