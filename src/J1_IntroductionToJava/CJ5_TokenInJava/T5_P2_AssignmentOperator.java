076eaf952a70cd8cb9e977fb98a742b30311d48c
RiyaGahtori <riyagahtori21@gmail.com>
2022-06-06 10:53:28 +0530

    Commit

---
+++ src/J1_IntroductionToJava/CJ5_TokenInJava/T5_P2_AssignmentOperator.java
@@ -0,0 +1,16 @@
+package J1_IntroductionToJava.CJ5_TokenInJava;
+
+public class T5_P2_AssignmentOperator {
+    public static void main(String[] args)
+    {
+        int a ;
+        a = 10; //Simple
+        System.out.println(a);
+
+        a+=10; // compound (a=a+10)
+        System.out.println(a);
+
+        a-=10; //Compound (a=a-10)
+        System.out.println(a);
+    }
+}
