26f68e32ed59faf10c2e9cbe67ac8db01b7cb4be
RiyaGahtori <riyagahtori21@gmail.com>
2022-06-17 18:22:29 +0530

    Commit

--- src/J1_IntroductionToJava/CJ7_Array/PracticeQuestions/P8.java
+++ src/J1_IntroductionToJava/CJ7_Array/PracticeQuestions/P8.java
@@ -1,7 +1,7 @@
 /*
 8. Write a program to count the frequency of each element of an array.
 Test Data :
-Input the number of elements to be stored in the array :3
+Input the number of elements to be stored in the array : 3
 Input 3 elements in the array :
 element - 0 : 25
 element - 1 : 12
@@ -20,9 +20,10 @@ public class P8 {
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
-        System.out.print("");
+        System.out.print("Input the number of elements to be stored in the array : ");
         int l = sc.nextInt();
         int arr[] = new int[l];
 
+        System.out.println("")
     }
 }
