8bc68647e793c622507a8dd3a41c77246beb61fa
RiyaGahtori <riyagahtori21@gmail.com>
2022-07-23 16:36:48 +0530

    Commit

---
+++ src/HackerEarth/Codemonk/C1_ArraysAndStrings/A1_1D/Tutorial.java
@@ -0,0 +1,30 @@
+/*
+Given the size and the elements of array A, print all the elements in reverse order.
+
+Input:
+First line of input contains, N - size of the array.
+Following N lines, each contains one integer, i{th} element of the array i.e. A[i].
+
+Output:
+Print all the elements of the array in reverse order, each element in a new line.
+--------------------------------------------------------------------------------------
+SAMPLE INPUT
+5
+4
+12
+7
+15
+9
+SAMPLE OUTPUT
+9
+15
+7
+12
+4
+
+ */
+
+package HackerEarth.Codemonk.C1_ArraysAndStrings.A1_1D;
+
+public class Tutorial {
+}
