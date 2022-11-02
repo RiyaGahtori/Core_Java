1219d5a2f1c973b1ae543e4571f8c8d8e3c3e0ae
RiyaGahtori <riyagahtori21@gmail.com>
2022-11-02 10:39:46 +0530

    Hello, World Hello, Java

--- src/HackerRank/Prepare/Java/Introduction/HR1_WelcomeToJava.java
+++ src/HackerRank/Prepare/Java/Introduction/HR1_WelcomeToJava.java
@@ -1,4 +1,16 @@
-//Practice printing to stdout.
+                                                            //Practice printing to stdout.
+/* Welcome to Java!
+Welcome to the world of Java! In this challenge, we practice printing to stdout.
+System.out.println("Hello, World.");
+System.out.println("Hello, Java.");
+
+Input Format
+There is no input for this challenge.
+
+Output Format
+You must print two lines of output:
+1. Print Hello, World. on the first line.
+2. Print Hello, Java. on the second line. */
 package HackerRank.Prepare.Java.Introduction;
 
 public class HR1_WelcomeToJava {
