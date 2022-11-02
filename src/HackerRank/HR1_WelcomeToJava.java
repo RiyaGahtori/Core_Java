c3b6e61878c5272eb755438644de0ab101af9000
RiyaGahtori <riyagahtori21@gmail.com>
2022-11-02 10:01:11 +0530

    Hello, World.

---
+++ src/HackerRank/HR1_WelcomeToJava.java
@@ -0,0 +1,10 @@
+//Practice printing to stdout.
+package HackerRank;
+
+public class HR1_WelcomeToJava {
+    public static void main(String[] args){
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
+
