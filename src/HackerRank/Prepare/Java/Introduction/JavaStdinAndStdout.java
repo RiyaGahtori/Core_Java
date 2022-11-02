4d0a5b13eb9e4e124af703301a6d78998c64daff
RiyaGahtori <riyagahtori21@gmail.com>
2022-11-02 10:15:35 +0530

    Java Stdin and Stdout

--- src/HackerRank/Prepare/Java/Introduction/JavaStdinAndStdout.java
+++ src/HackerRank/Prepare/Java/Introduction/JavaStdinAndStdout.java
@@ -1,4 +1,20 @@
+//Get started with standard input and output.
 package HackerRank.Prepare.Java.Introduction;
+import java.util.*;
 
 public class JavaStdinAndStdout {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
 }
+
+
+
