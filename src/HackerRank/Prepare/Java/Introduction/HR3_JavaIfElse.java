d2105fc67771f75e568da902f9795cf3e83c2932
RiyaGahtori <riyagahtori21@gmail.com>
2022-11-02 10:37:46 +0530

    if-else conditional statements!

--- src/HackerRank/Prepare/Java/Introduction/HR3_JavaIfElse.java
+++ src/HackerRank/Prepare/Java/Introduction/HR3_JavaIfElse.java
@@ -1,4 +1,4 @@
-//Practice using if-else conditional statements!
+                                                          //Practice using if-else conditional statements!
 /*
 Task
 Given an integer, n, perform the following conditional actions:
@@ -6,11 +6,13 @@ If n is odd, print Weird
 If n is even and in the inclusive range of 2 to 5, print Not Weird
 If n is even and in the inclusive range of 6 to 20, print Weird
 If n is even and greater than 20, print Not Weird
-Complete the stub code provided in your editor to print whether or not n is weird.
+
 Input Format
 A single line containing a positive integer, n.
+
 Constraints
 1 <= n <= 100
+
 Output Format
 Print Weird if the number is weird; otherwise, print Not Weird. */
 package HackerRank.Prepare.Java.Introduction;
