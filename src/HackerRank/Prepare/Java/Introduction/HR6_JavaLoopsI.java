327fda8160b390e28a22ac9dc4e8ca33f9ab2252
RiyaGahtori <riyagahtori21@gmail.com>
2022-11-06 08:10:01 +0530

    Let's talk about loops.

--- src/HackerRank/Prepare/Java/Introduction/HR6_JavaLoopsI.java
+++ src/HackerRank/Prepare/Java/Introduction/HR6_JavaLoopsI.java
@@ -1,5 +1,22 @@
                                                                       //Let's talk about loops
+/*Task
+Given an integer, N , print its first 10 multiples. Each multiple N x i (where 1<=i<=10) should be printed on a new line in the form: N x i = result.
 
+Sample Input
+2
+
+Sample Output
+2 x 1 = 2
+2 x 2 = 4
+2 x 3 = 6
+2 x 4 = 8
+2 x 5 = 10
+2 x 6 = 12
+2 x 7 = 14
+2 x 8 = 16
+2 x 9 = 18
+2 x 10 = 20
+ */
 package HackerRank.Prepare.Java.Introduction;
 
 public class HR6_JavaLoopsI {
