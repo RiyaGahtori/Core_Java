2e92db9035b546a374fb3f9118dd7bd99a60f77f
RiyaGahtori <riyagahtori21@gmail.com>
2022-11-06 08:30:19 +0530

    Use loops to find sum of a series.

---
+++ src/HackerRank/Prepare/Java/Introduction/HR7_JavaLoopsII.java
@@ -0,0 +1,6 @@
+                                                                    //Use loops to find sum of a series
+
+package HackerRank.Prepare.Java.Introduction;
+
+public class HR7_JavaLoopsII {
+}
